8c6cf1172f2c6090ab4dd42ee8cfb51c9f067704
Kawaiivee <Kawaiivee@gmail.com>
2024-09-23 23:28:47 -0500

    Feature25: Added more examples

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Text.Json.Serialization;
 using WebApplication1.Models;
+using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebApplication1.Models.Requests;
 
 namespace WebApplication1.Controllers
 {
@@ -8,13 +12,36 @@ namespace WebApplication1.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        private List<string> premiumUsers = [
+            "ramir"
+        ];
+
+        private static IEnumerable<string> pokemonDb = Enumerable.Empty<string>();
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        [Route("{text?}")]
+        public IActionResult Index(string text)
         {
+            text = text ?? "Home";
+            ViewData["Title"] = text;
+            ViewData["IsPremiumUser"] = premiumUsers.Contains(text) ? "PREMIUM SUB" : "BROKE BOY";
+            ViewData["pokemon"] = pokemonDb.FirstOrDefault();
+            return View();
+        }
+
+        [HttpPost]
+        [Route("pokemon/create")]
+        public IActionResult Index([FromBody] PokemonRequest pokemon)
+        {
+            var name = pokemon.Name;    // -> get{ ... } is called here
+            pokemon.Name = "eevee";
+            var pokemonText = JsonConvert.SerializeObject(pokemon);
+            pokemonDb = pokemonDb.Append(pokemonText);
             return View();
         }
 
@@ -26,6 +53,13 @@ namespace WebApplication1.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            PokemonRequest updatedPR = new PokemonRequest()
+            {
+                CreatedDate = DateTime.UtcNow,
+                Name = "ramir",
+            };
+
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
---
+++ WebApplication1/Models/Requests/PokemonRequest.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace WebApplication1.Models.Requests
+{
+    public class PokemonRequest
+    {
+        private readonly List<int> DEFAULT_EMPTY_MOVELIST = Enumerable.Empty<int>().ToList();
+        public PokemonRequest() { }
+        public PokemonRequest(string name, string type, List<int> moves)
+        {
+            Name = name;
+            Type = type;
+            Moves = DEFAULT_EMPTY_MOVELIST;
+        }
+
+        private int _points;
+
+        [JsonPropertyName("points")]
+        [NotMapped]
+        public string Money
+        {
+            get
+            {
+                return _points.ToString("$#.##");
+            }
+            set
+            {
+                _points = Convert.ToInt32(value);
+            }
+        }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+        [JsonPropertyName("moves")]
+        public List<int> Moves { get; set; }
+        [JsonPropertyName("createdDate")]
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        [JsonPropertyName("updatedDate")]
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
