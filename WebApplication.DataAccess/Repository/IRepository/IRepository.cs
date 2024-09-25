d49d2d089ad002a20a84e8f11f7fecfb0385cada
Mathias Kniesel <kniesel1080@gmail.com>
2024-09-25 00:05:44 -0500

    added Repo pattern to solution, Repo interface and directory, lastly the implementation

---
+++ WebApplication.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication.DataAccess.Repository.IRepository
+{
+    //typically, when we are working with generic interface where we do not know what the class type
+    //will be we say it will be a generic type <T> ......
+    internal interface IRepository<T> where T : class
+    {
+        //T - Category
+        T Get(Expression<Func<T, bool>> filter);
+        void Add(T entity);
+        
+        //update usually has specific scenario implementations thus can be overrided in location of use
+        //void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entity);
+        
+    }
+}
