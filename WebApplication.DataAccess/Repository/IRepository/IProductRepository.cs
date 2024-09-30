1b772f420ec19498a3bb5cbb42540aec85a5fbb0
Mathias Kniesel <kniesel1080@gmail.com>
2024-09-30 01:17:00 -0500

    Created interface and concrete product repo, also included within unitofwork

---
+++ WebApplication.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.Models.Models;
+
+namespace WebApplication.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        //TODO: ADD MORE SPECIFIC FUNCTIONALITY
+        void Update(Product obj);
+
+
+
+    }
+}
---
+++ WebApplication.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication.DataAccess.Data;
+using WebApplication.DataAccess.Repository.IRepository;
+using WebApplication.Models.Models;
+
+namespace WebApplication.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+
+
+    }
+}
