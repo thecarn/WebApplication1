f319ca5bcd0db9274b097b3acd05e1c99f9c18b6
Mathias Kniesel <kniesel1080@gmail.com>
2024-09-30 02:08:23 -0500

    Fixed typo in partial view, fixed missed definition in IUnitOfWork

--- WebApplication.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ WebApplication.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,7 +9,7 @@ namespace WebApplication.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
-
+        IProductRepository Product { get; }
         void Save();
     }
 }
