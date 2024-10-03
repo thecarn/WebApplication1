ad3dad6ceacb683782926589757eae45154e3b8e
Mathias Kniesel <kniesel1080@gmail.com>
2024-10-03 01:45:15 -0500

    Added home view and uow in homecontroller

--- WebApplication1/Areas/Customer/Controllers/HomeController.cs
+++ WebApplication1/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using WebApplication.DataAccess.Repository.IRepository;
 using WebApplication.Models.Models;
 
 
@@ -9,15 +10,18 @@ namespace WebApplication1.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");     
+            return View(productList);
         }
 
         public IActionResult Privacy()
