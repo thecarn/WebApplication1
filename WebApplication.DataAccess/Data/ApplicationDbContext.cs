588657c23379e1caf5643a31b932c9868a66c2b4
Mathias Kniesel <kniesel1080@gmail.com>
2024-10-04 09:03:42 -0500

    removed identity made appdbcontext file

--- WebApplication.DataAccess/Data/ApplicationDbContext.cs
+++ WebApplication.DataAccess/Data/ApplicationDbContext.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using WebApplication.Models.Models;
 
 
 namespace WebApplication.DataAccess.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
         {
