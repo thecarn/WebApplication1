0161b1135f5c5c71b0bdd584bf018134caecc07f
Mathias Kniesel <kniesel1080@gmail.com>
2024-10-06 07:43:27 -0500

    Added applicationUser model for identity

--- WebApplication.DataAccess/Data/ApplicationDbContext.cs
+++ WebApplication.DataAccess/Data/ApplicationDbContext.cs
@@ -16,6 +16,7 @@ namespace WebApplication.DataAccess.Data
         public DbSet<Category> Categories {  get; set; }
         public DbSet<Product> Products { get; set; }
 
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             //NEED THIS IF USING ONMODELCREATING AND USING IDENTITY
